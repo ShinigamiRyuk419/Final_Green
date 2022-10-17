30091361f24a08c75e065938c351034668f03746
Aron Balais <aaron.balais@student.passerellesnumeriques.org>
2022-10-17 15:07:37 +0800

    initialize

---
+++ Sd_205/app/Models/Register.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+use DB;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Register extends Model
+{
+    function create_account($account){
+        DB::table('accounts')->insert([
+            'email' => $account->email,
+            'password' => $account->password
+        ]);
+    }
+}
